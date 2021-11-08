1f05a967f66829192dc5f2d4316b058b882bbf5c
MinCheTsai <minche@softchef.com>
2021-11-08 16:10:42 +0800

    Update comments

--- lambda-assets/pwa-deployment/app.ts
+++ lambda-assets/pwa-deployment/app.ts
@@ -19,15 +19,18 @@ export async function handler(event: Object): Promise<any> {
       ContentType: 'text/html',
       Body: `<html>
   <body>
-    <h3>This is PWA website</h3>
+    <h3>This is PWA PoC website</h3>
     <h4>Iframe from Proxy URL https://${proxyUrl}</h4>
-    <p>The proxy URL is removed x-frame-options header by CloudFront & Lambda@Edge, it's embed content success.</p>
+    <p>The proxy URL is add Content-Security-Policy by CloudFront & Lambda@Edge, it's embed content success.</p>
     <iframe src="https://${proxyUrl}" width="100%" height="250"></iframe>
-    <p>The origin URL is added x-frame-options header, it's embed content failed.</p>
+    <hr />
     <h4>Iframe from Origin URL https://${originUrl}</h4>
+    <p>The origin URL is added x-frame-options header, it's embed content failed.</p>
     <iframe src="https://${originUrl}" width="100%" height="250"></iframe>
+    <hr />
     <h4>Iframe from Acer Origin CloudFront URL https://${acerOriginUrl}</h4>
     <iframe src="https://${acerOriginUrl}" width="100%" height="250"></iframe>
+    <hr />
     <h4>Iframe from Acer Add Headers URL https://${acerAddHeadersUrl}</h4>
     <iframe src="https://${acerAddHeadersUrl}" width="100%" height="250"></iframe>
   </body>
